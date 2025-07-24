1a9aeed4b45d16d0669b1d5b419b9e2704b60590
username1066648304 <jsclambler@gmail.com>
2025-07-24 15:16:10 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,31 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function (e) {
+document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
-  const username = e.target[0].value;
-  const password = e.target[1].value;
-  const remember = document.getElementById('remember').checked;
-
-  console.log("Login attempt:", { username, password, remember });
-
-  // Simpan ke localStorage jika Remember Me
-  if (remember) {
-    localStorage.setItem('savedUsername', username);
-    localStorage.setItem('savedPassword', password);
-  } else {
-    localStorage.removeItem('savedUsername');
-    localStorage.removeItem('savedPassword');
-  }
-
-  // Redirect ke index.html
-  window.location.href = "index.html";
+  alert("Login system belum dihubungkan ke API.");
 });
-
-// Auto isi jika Remember Me pernah digunakan
-window.onload = function () {
-  const savedUsername = localStorage.getItem('savedUsername');
-  const savedPassword = localStorage.getItem('savedPassword');
-  if (savedUsername && savedPassword) {
-    document.querySelector('input[type="text"]').value = savedUsername;
-    document.querySelector('input[type="password"]').value = savedPassword;
-    document.getElementById('remember').checked = true;
-  }
-};
