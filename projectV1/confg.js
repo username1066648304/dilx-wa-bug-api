8146aeecab4bd67a75da3cc8da91f7296df67184
username1066648304 <jsclambler@gmail.com>
2025-07-24 21:09:32 +0800

    Update confg.js

--- projectV1/confg.js
+++ projectV1/confg.js
@@ -1,20 +1,8 @@
-// config.js
-
 module.exports = {
-  // Token Telegram Bot
-  TELEGRAM_BOT_TOKEN: '7392047062:AAGSNxMUGr15MHG7hJ5GUXwdUnjmczGdiUw',
-
-  // Telegram Owner ID
-  OWNER_ID: '7339008277',
-
-  // GitHub JSON Settings
-  GITHUB_TOKEN: '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', // simpan rahsia
-  REPO_OWNER: 'dilxvxii',     // ganti dgn username GitHub anda
-  REPO_NAME: 'ultra-database', // nama repo GitHub anda
-  FILE_PATH: 'users.json',     // path fail JSON dalam repo
-  BRANCH: 'main',              // nama branch utama
-
-  // Sistem
-  BRAND_NAME: 'Ultra',
-  DEVELOPER_NAME: 'dilxVXII'
+  ADMIN_ROLE: "owner",
+  SESSION_EXPIRE_DAYS: 30,
+  TELEGRAM: {
+    BOT_TOKEN: "7392047062:AAGSNxMUGr15MHG7hJ5GUXwdUnjmczGdiUw",
+    OWNER_ID: "7339008277"
+  }
 };
