97cf84136664067cb867f901a39c33b9c6cbaa48
username1066648304 <jsclambler@gmail.com>
2025-07-24 17:05:45 +0800

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,13 +1,12 @@
-
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  const username = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
-  const errorMsg = document.getElementById("error-msg");
-
-  if (username === "dilx" && password === "1") {
-    window.location.href = "dashboard.html";
-  } else {
-    errorMsg.textContent = "Username atau Password salah!";
-  }
-});
+function createUser() {
+  alert('Create user triggered');
+}
+function triggerBug() {
+  alert('Trigger bug triggered');
+}
+function addBot() {
+  alert('Add pairing bot triggered');
+}
+function addFunction() {
+  alert('Add async bug JS triggered');
+}
