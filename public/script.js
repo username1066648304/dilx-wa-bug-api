9176a625c6075346ed4c6d43607563d72093575c
username1066648304 <jsclambler@gmail.com>
2025-07-24 16:15:03 +0800

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,21 +1,13 @@
 
-document.getElementById("login-form").addEventListener("submit", function (e) {
+document.getElementById("loginForm").addEventListener("submit", function(e) {
   e.preventDefault();
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
   const errorMsg = document.getElementById("error-msg");
 
   if (username === "dilx" && password === "1") {
-    localStorage.setItem("loggedIn", "true");
     window.location.href = "dashboard.html";
   } else {
-    errorMsg.textContent = "Username atau password salah!";
-    errorMsg.style.color = "red";
+    errorMsg.textContent = "Username atau Password salah!";
   }
 });
-
-window.onload = () => {
-  if (window.location.pathname.includes("dashboard.html") && localStorage.getItem("loggedIn") !== "true") {
-    window.location.href = "login.html";
-  }
-};
