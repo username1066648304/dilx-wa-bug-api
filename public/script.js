44d7b52ea975735386de9b5f9d155fa265c8b48b
username1066648304 <jsclambler@gmail.com>
2025-07-24 17:30:37 +0800

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,12 +1,1 @@
-function createUser() {
-  alert('Create user triggered');
-}
-function triggerBug() {
-  alert('Trigger bug triggered');
-}
-function addBot() {
-  alert('Add pairing bot triggered');
-}
-function addFunction() {
-  alert('Add async bug JS triggered');
-}
+// Future login functionality
